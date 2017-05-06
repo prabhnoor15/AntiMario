988b0552e3201b182b6629a16b84ca49ddaa6444
yuanxinwang <alexyuanxinwang@gmail.com>
2017-05-05 23:37:20 -0400

    Basic Setting
    The character and background image will come up soon.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float jumpPower = 30f;
+    public float maxSpeed = 5f;
+    private bool facingRight;
+
+    private bool IsOnGround = true;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(move * maxSpeed, rb.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col != null)
+        {
+            IsOnGround = true;
+        }
+    }
+}
